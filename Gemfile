990a3000aa8c45584001174877af468c62f84231
Kristian Mandrup <kmandrup@gmail.com>
2011-08-24 19:44:59 +0200

    new dependency structure

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source :rubygems
+
+gem "require_all",   "~> 1.2.0"
+gem "rspec",         ">= 2.4.1"
+gem "sugar-high",    ">= 0.6.0"
+gem "activesupport", ">= 3.0.1"
+
--- lib/file-spec.rb
+++ lib/file-spec.rb
@@ -1,8 +1,8 @@
 require 'rspec'
-require 'require_all'      
+require 'require_all'
 require 'fileutils'
 require 'tmpdir'
-require 'active_support/inflector'      
+require 'active_support/inflector'
 require 'sugar-high'
 
 module RSpec
@@ -15,5 +15,5 @@ require_all File.dirname(__FILE__) + '/file_spec/matchers'
 require_all File.dirname(__FILE__) + '/file_spec/file_helpers'
 
 RSpec.configure do |config|
-  config.include(RSpec::FileMatchers)  
+  config.include(RSpec::FileMatchers)
 end
